1a31f02c1f4e99723f277fba97577bf58b56893e
Sergey <sonicbsv@gmail.com>
2017-10-23 22:52:12 +0300

    Add missing  apns config (Update)

--- nitrogen_markw.mk
+++ nitrogen_markw.mk
@@ -19,7 +19,7 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Provide meaningful APN configuration
-PRODUCT_COPY_FILES := device/xiaomi/kenzo/configs/apns-full-conf.xml:system/etc/apns-conf.xml
+PRODUCT_COPY_FILES := device/xiaomi/markw/configs/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
