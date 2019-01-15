acb994f120bf1e2e36cc33122d0e0c27a025a778
Sergey <sonicbsv@gmail.com>
2019-01-15 14:22:33 +0300

    Pixel DUST Rom

---
+++ pixeldust_markw.mk
@@ -0,0 +1,59 @@
+# Copyright (C) 2018 The Pixel Dust Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Release name
+PRODUCT_RELEASE_NAME := Redmi 4
+export TARGET_DEVICE := markw
+
+# Inherit AOSP device configuration for markw.
+$(call inherit-product, device/xiaomi/markw/aosp_markw.mk)
+
+# Include common PixelDust stuff
+include vendor/pixeldust/configs/pixeldust_phone.mk
+
+# Include optional stuff (e.g. prebuilt apps)
+include vendor/pixeldust/configs/system_optional.mk
+
+# Google Apps
+$(call inherit-product, vendor/googleapps/googleapps.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := pixeldust_markw
+PRODUCT_BRAND := xiaomi
+PRODUCT_DEVICE := markw
+PRODUCT_MODEL := Redmi 4
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=markw \
+    PRIVATE_BUILD_DESC="markw-user 6.0.1 MMB29M V9.6.2.0.MBECNFD release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/markw/markw:6.0.1/MMB29M/V9.6.2.0.MBECNFD:user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.pixeldust.maintainer="SonicBSV"
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/pixeldust/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Use SDCLANG
+TARGET_USE_SDCLANG := true
+
+# Google VR
+PRODUCT_PACKAGES += \
+    libdvr_loader \
+    com.google.vr.platform \
+    com.google.vr.platform.xml
+
