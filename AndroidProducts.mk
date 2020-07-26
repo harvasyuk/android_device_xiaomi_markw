207b88a987b91a35e9c5d5be7afc578e2eeddc52
SonicBSV <sonicbsv@gmail.com>
2020-07-26 17:35:00 +0300

    AOSPA CAF Q

---
+++ AndroidProducts.mk
@@ -0,0 +1,5 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/pa_markw.mk
+
+COMMON_LUNCH_CHOICES := \
+    pa_markw-userdebug
---
+++ pa_markw.mk
@@ -0,0 +1,35 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
+
+# Inherit from markw device
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+TARGET_DISABLES_GAPPS := false
+TARGET_USES_HARDWARE_KEYS := true
+TARGET_USES_AOSP_CAMERA := true
+
+# Boot animation
+TARGET_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+IS_PHONE := true
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := Xiaomi
+PRODUCT_NAME := pa_markw
+BOARD_VENDOR := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_DEVICE := markw
+PRODUCT_MODEL := Redmi 4 Prime
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_VENDOR_PRODUCT_NAME := markw
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="markw-user 6.0.1 MMB29M V10.2.2.0.MBEMIXM release-keys" \
+    DEVICE_MAINTAINERS="SonicBSV"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Xiaomi/markw/markw:6.0.1/MMB29M/V10.2.2.0.MBEMIXM:user/release-keys
